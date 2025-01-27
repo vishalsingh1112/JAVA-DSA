7728a5270904743c8d5e4327ff8384211bf288bd
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-27 22:59:59 +0530

    changes

--- Takinginputs.java
+++ Takinginputs.java
@@ -4,14 +4,14 @@ public class Takinginputs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // System.out.println("enter your name ");
-        // String name = sc.nextLine();
-        // System.out.println("your name is "+name );
+        System.out.println("enter your name "); 
+        String name = sc.nextLine();
+        System.out.println("your name is "+name );
 
 
-        // System.out.println("enter your roll no");
-        // int rollno= sc.nextInt();
-        // System.out.println("your roll no is "+rollno);
+        System.out.println("enter your roll no");
+        int rollno= sc.nextInt();
+        System.out.println("your roll no is "+rollno);
 
 
 
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+System.out.println("enter your name ");
+        String name = sc.nextLine();
+        System.out.println("your name is "+name );
+
+
+        System.out.println("enter your roll no");
+        int rollno= sc.nextInt();
+        System.out.println("your roll no is "+rollno);
